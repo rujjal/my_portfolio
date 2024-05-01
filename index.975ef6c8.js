3346f83157a5bfe390f49f574ce44d3584648f8d
Rujjal Sada <85438552+rujjal@users.noreply.github.com>
2024-05-01 13:42:52 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27090,11 +27090,11 @@ var _stylesCss = require("./styles.css");
     name: "Rujjal Sada",
     title: "Data Science Student",
     email: "rujjalsada25@gmail.com",
-    gitHub: "/rujjal",
-    instagram: "/ruzzall",
-    linkedIn: "/rujjal-sada-21b7b8164",
+    gitHub: "rujjal",
+    instagram: "ruzzall",
+    linkedIn: "rujjal-sada-21b7b8164",
     medium: "",
-    twitter: "/ruzzall",
+    twitter: "ruzzall",
     youTube: "Code"
 };
 const primaryColor = "#4E567E";
