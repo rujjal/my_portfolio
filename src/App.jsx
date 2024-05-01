505c24ee4e87c599521ebe2d964c8185dcc08910
Rujjal Sada <85438552+rujjal@users.noreply.github.com>
2024-05-01 13:30:03 +0000

    added personal details

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rujjal Sada",
+  title: "Data Science Student",
+  email: "rujjalsada25@gmail.com",
+  gitHub: "https://github.com/rujjal",
+  instagram: "https://www.instagram.com/ruzzall/",
+  linkedIn: "https://www.linkedin.com/in/rujjal-sada-21b7b8164/",
   medium: "",
-  twitter: "microsoft",
+  twitter: "https://twitter.com/ruzzall",
   youTube: "Code",
 };
 
